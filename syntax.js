2c808cad571a416b2c8a1d8dc50768587f216a16
Aradlo <49364288+aradlo@users.noreply.github.com>
2021-08-18 12:58:02 +0700

    Update and rename syntax.css to syntax.js

---
+++ syntax.js
@@ -0,0 +1,3 @@
+// Highlighter
+$('i[rel="pre"]').replaceWith(function(){return $("<pre><code>"+$(this).html()+"</code></pre>")});for(var pres=document.querySelectorAll("pre,code,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
+function downloadJSAtOnload(){var e=document.createElement("script");e.src="https://cdn.statically.io/gh/demonlordonline/Winfoku/3d1ca789/Highlighter.js",document.body.appendChild(e)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
